353dd40386d1ee9476a9c9468363ee81dcadea88
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-08-06 12:59:02 -0300

    putting labels in views

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    
+    
+    @IBOutlet weak var conditionLabel: UILabel!
+    
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
